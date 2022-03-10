4a2647f1794e8f5669df1e9ca09d4f3bdb2916e9
Jeslin26 <94427440+Jeslin26@users.noreply.github.com>
2022-03-10 12:10:08 +0530

    Add files via upload

---
+++ Jeslin21_Lab_Exam/asort.php
@@ -0,0 +1,18 @@
+<?php
+print_r("the original array is ");
+$a=array("chandra","bincy","aaradhya");
+print_r($a);
+echo ("<br> Sorted list <br> ");
+asort($a);
+print_r($a);
+echo ("<br> descending order <br> ");
+arsort($a);
+print_r($a);
+?>
+
+
+
+
+
+
+
